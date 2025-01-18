d30c0f59b0b1a5cde8e821b687a0ae890a2c4112
Isaac <newdevcoding@gmail.com>
2025-01-18 13:51:00 -0500

    changed layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
+
 import "./globals.css";
 import { ThemeProvider } from "./provider";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Isaac's Portfolio",
-  description: "A showcase for all my work",
+  title: "Adrian's Portfolio",
+  description: "Modern & Minimal JS Mastery Portfolio",
 };
 
 export default function RootLayout({
@@ -24,10 +17,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/jsm-logo.png" sizes="any" />
+      </head>
+      <body className={inter.className}>
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
@@ -40,3 +34,46 @@ export default function RootLayout({
     </html>
   );
 }
+
+// import type { Metadata } from "next";
+// import { Geist, Geist_Mono } from "next/font/google";
+// import "./globals.css";
+// import { ThemeProvider } from "./provider";
+
+// // const geistSans = Geist({
+// //   variable: "--font-geist-sans",
+// //   subsets: ["latin"],
+// // });
+
+// // const geistMono = Geist_Mono({
+// //   variable: "--font-geist-mono",
+// //   subsets: ["latin"],
+// // });
+
+// export const metadata: Metadata = {
+//   title: "Isaac's Portfolio",
+//   description: "A showcase for all my work",
+// };
+
+// export default function RootLayout({
+//   children,
+// }: Readonly<{
+//   children: React.ReactNode;
+// }>) {
+//   return (
+//     <html lang="en">
+//       <body
+//         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+//       >
+//         <ThemeProvider
+//           attribute="class"
+//           defaultTheme="dark"
+//           enableSystem
+//           disableTransitionOnChange
+//         >
+//           {children}
+//         </ThemeProvider>
+//       </body>
+//     </html>
+//   );
+// }
